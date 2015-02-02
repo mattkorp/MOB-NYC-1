471c1fc0db2057fa10a3bac941525cf33f0f4eea
matt-korp <matt@korp.me>
2015-02-01 22:57:38 -0500

    Homework 4

--- Assesments/04/Lesson04/ArrayViewController.swift
+++ Assesments/04/Lesson04/ArrayViewController.swift
@@ -2,25 +2,170 @@
 //  ViewController.swift
 //  Lesson04
 //
-//  Created by Rudd Taylor on 9/28/14.
-//  Copyright (c) 2014 General Assembly. All rights reserved.
+//
+//TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
+//
+//TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
 //
 
 import UIKit
 
-class ArrayViewController: UIViewController {
+class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    var savedElements: [String]?
+    
+    var tableView = UITableView()
+    var tableArray = [String]()
+    var userInputField = UITextField()
+    
+    var minX = CGFloat()
+    var minY = CGFloat()
+    var width = CGFloat()
+    var height = CGFloat()
+    var rowHeight: CGFloat = 40
+    var backgroundColor = UIColor(red:0.16, green:0.17, blue:0.21, alpha:1)
+    
+    // MARK: View
+    
+    init(frame: CGRect) {
+        super.init(nibName: nil, bundle: nil)
+        
+        minX = frame.minX
+        minY = frame.minY
+        width = frame.width
+        height = frame.height
+        
+    }
 
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
-        */
-        
-        
-        /*
-        TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
-        */
+        readPlist()
+        setupNavBar()
+        setupTableView()
+        setupTextField()
+    
+        tableView.reloadData()
+    
     }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.All.rawValue)
+    }
+    
+    func readPlist() {
+        var docsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        var path = docsDirectory.stringByAppendingPathComponent("array.plist")
+        if let dataIn = NSArray(contentsOfFile: path) {
+            tableArray = dataIn as [String]
+        }
+    
+    }
+    
+    // MARK: NavBar
+    
+    func setupNavBar() {
+        let nextBarButton = UIBarButtonItem(title: "Map", style: .Plain, target: self, action: "mapButtonPressed")
+        self.navigationItem.rightBarButtonItem = nextBarButton
+        let saveBarButton = UIBarButtonItem(title: "Save", style: .Plain, target: self, action: "saveToPlist")
+        self.navigationItem.leftBarButtonItem = saveBarButton
+        self.navigationItem.title = "Array"
+    }
+    
+    func mapButtonPressed() {
+        // TODO: Is this the best way to get Orientation?
+        var frame = CGRect()
+        var orientation = UIDevice.currentDevice().orientation
+        if UIDeviceOrientationIsPortrait(orientation) {
+            frame = CGRect(x: minX, y: minY, width: width, height: height)
+        } else if UIDeviceOrientationIsLandscape(orientation) {
+            frame = CGRect(x: minX, y: minY, width: height, height: width)
+        }
+        self.navigationController?.pushViewController(MapViewController(frame: frame), animated: true)
+    }
+    
+    func saveToPlist() {
+        var docsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        var path = docsDirectory.stringByAppendingPathComponent("array.plist")
+        var fileManager = NSFileManager.defaultManager()
+        if (!fileManager.fileExistsAtPath(path)) {
+            var bundle: NSString = NSBundle.mainBundle().pathForResource("array", ofType: "plist")!
+            fileManager.copyItemAtPath(bundle, toPath: path, error: nil)
+        }
+        var data = NSArray(array: tableArray as NSArray)
+        data.writeToFile(path, atomically: true)
+    }
+    
+    // MARK: TextField
+    
+    func setupTextField() {
+        userInputField = UITextField(frame: CGRectMake(0, 0, width, rowHeight))
+        userInputField.autoresizingMask = UIViewAutoresizing.FlexibleWidth
+        userInputField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        userInputField.backgroundColor = UIColor.lightGrayColor()
+        userInputField.layer.cornerRadius = 10
+        userInputField.placeholder = " Enter Something.."
+        userInputField.delegate = self
+        tableView.tableHeaderView?.addSubview(userInputField)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        tableArray.append(userInputField.text)
+        userInputField.text = ""
+        userInputField.resignFirstResponder()
+        tableView.reloadData()
+        return true
+    }
+    
+    // MARK: TableView Management
+    
+    func setupTableView() {
+        tableView = UITableView(frame: CGRect(x: minX, y: minY, width: width, height: height))
+        tableView.tableHeaderView = UIView(frame: CGRectMake(0, 0, width, rowHeight))
+        tableView.autoresizingMask = (UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight)
+        tableView.rowHeight = rowHeight
+        tableView.backgroundColor = backgroundColor
+        tableView.separatorInset = UIEdgeInsetsZero
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Array") as? UITableViewCell ?? UITableViewCell(style: .Default, reuseIdentifier: "Array")
+        let row = indexPath.row
+        cell.textLabel?.text = tableArray[indexPath.row]
+        cell.backgroundColor = backgroundColor
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        return cell
+    }
+
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.separatorInset = UIEdgeInsetsZero
+        cell.superview?.preservesSuperviewLayoutMargins = false
+        cell.layoutMargins = UIEdgeInsetsZero
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            tableArray.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+    
 }
 
--- Assesments/04/Lesson04/MapViewController.swift
+++ Assesments/04/Lesson04/MapViewController.swift
@@ -2,27 +2,275 @@
 //  MapViewController.swift
 //  Lesson04
 //
-//  Created by Rudd Taylor on 9/28/14.
-//  Copyright (c) 2014 General Assembly. All rights reserved.
+/*
+
+TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
+*/
+
+/*
+TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
+*/
+
+/*
+TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
+*/
 //
 
 import UIKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+    
+    var tableView = UITableView()
+    var keyTextField = UITextField()
+    var valueTextField = UITextField()
+    var keyValueDictionaryArray = OrderedDictionary<String,String>()
+    var enterButton = UIButton()
+    
+    var minX = CGFloat()
+    var minY = CGFloat()
+    var width = CGFloat()
+    var height = CGFloat()
+    
+    var rowHeight: CGFloat = 40
+    var backgroundColor = UIColor(red:0.16, green:0.17, blue:0.21, alpha:1)
+    var backgroundColorKeyboardShow = UIColor(red:0.46, green:0.86, blue:1, alpha:1)
+    var backgroundColorKeyboardHide = UIColor(red:1, green:0.41, blue:0.33, alpha:1)
+        
+    // MARK: Init
+    
+    override init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    init(frame: CGRect) {
+        super.init(nibName: nil, bundle: nil)
+        
+        minX = frame.minX
+        minY = frame.minY
+        width = frame.width
+        height = frame.height
+        
+        self.navigationItem.title = "Map"
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
-        */
+        setupTableView()
+        setupUserInputFields()
+        setupInputFieldsConstraints()
         
-        /*
-        TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
-        */
+        NSNotificationCenter.defaultCenter().addObserverForName(UIKeyboardDidShowNotification, object: nil, queue: nil) { (notification: NSNotification!) -> Void in
+            self.keyTextField.backgroundColor = self.backgroundColorKeyboardShow
+            self.valueTextField.backgroundColor = self.backgroundColorKeyboardShow
+        }
+
+        NSNotificationCenter.defaultCenter().addObserverForName(UIKeyboardDidHideNotification, object: nil, queue: nil) { (notification: NSNotification!) -> Void in
+            self.keyTextField.backgroundColor = self.backgroundColorKeyboardHide
+            self.valueTextField.backgroundColor = self.backgroundColorKeyboardHide
+        }
         
-        /*
-        TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
-        */
+    }
+    
+    func setupTableView() {
+        tableView = UITableView(frame: CGRect(x: minX, y: minY, width: width, height: height))
+        tableView.tableHeaderView = UIView(frame: CGRect(x: minX, y: minY, width: width, height: rowHeight))
+        tableView.autoresizingMask = (UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight)
+        tableView.backgroundColor = backgroundColor
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorInset = UIEdgeInsetsZero
+        //tableView.separatorColor = UIColor.clearColor()
+        //tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        self.view.addSubview(tableView)
+    }
+    
+    func setupUserInputFields() {
+        var textFieldWidth = (width / 2) - rowHeight
+        var valueX = minX + (textFieldWidth / 2)
+        var buttonX = minX + textFieldWidth
+        
+        keyTextField = UITextField(frame: CGRect(x: minX, y: minY, width: textFieldWidth, height: rowHeight))
+        keyTextField.delegate = self
+        keyTextField.borderStyle = UITextBorderStyle.Line
+        keyTextField.backgroundColor = UIColor.lightGrayColor()
+        keyTextField.placeholder = " KEY"
+        keyTextField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        keyTextField.layer.cornerRadius = 10
+        keyTextField.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        valueTextField = UITextField(frame: CGRect(x: valueX, y: minY, width: textFieldWidth, height: rowHeight))
+        valueTextField.delegate = self
+        valueTextField.borderStyle = UITextBorderStyle.Line
+        valueTextField.backgroundColor = UIColor.lightGrayColor()
+        valueTextField.placeholder = " VALUE"
+        valueTextField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        valueTextField.layer.cornerRadius = 10
+        valueTextField.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        enterButton = UIButton(frame: CGRect(x: buttonX, y: minY, width: rowHeight, height: rowHeight))
+        enterButton.layer.borderWidth = 1
+        enterButton.setAttributedTitle(NSAttributedString(string: "+"), forState: .Normal)
+        enterButton.backgroundColor = UIColor.lightGrayColor()
+        enterButton.layer.cornerRadius = 10
+        enterButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        enterButton.addTarget(self, action: Selector("enterButtonPressed"), forControlEvents: .TouchUpInside)
+        
+        tableView.tableHeaderView?.addSubview(keyTextField)
+        tableView.tableHeaderView?.addSubview(valueTextField)
+        tableView.tableHeaderView?.addSubview(enterButton)
+    }
+    
+    func setupInputFieldsConstraints() {
+        // Horizontal Layout
+        var keyTextLeft = NSLayoutConstraint(
+            item: keyTextField,
+            attribute: .Left,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Left,
+            multiplier: 1.0,
+            constant: 0.0)
+        var valueTextLeft = NSLayoutConstraint(
+            item: valueTextField,
+            attribute: .Left,
+            relatedBy: .Equal,
+            toItem: keyTextField,
+            attribute: .Right,
+            multiplier: 1.0,
+            constant: 0.0)
+        var valueTextRight = NSLayoutConstraint(
+            item: valueTextField,
+            attribute: .Right,
+            relatedBy: .Equal,
+            toItem: enterButton,
+            attribute: .Left,
+            multiplier: 1.0,
+            constant: 0.0)
+        var buttonRight = NSLayoutConstraint(
+            item: enterButton,
+            attribute: .Right,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Right,
+            multiplier: 1.0,
+            constant: 0.0)
+        var keyValueEqualWidths = NSLayoutConstraint(
+            item: keyTextField,
+            attribute: NSLayoutAttribute.Width,
+            relatedBy: .Equal,
+            toItem: valueTextField,
+            attribute: NSLayoutAttribute.Width,
+            multiplier: 1.0,
+            constant: 0.0)
+
+        tableView.tableHeaderView?.addConstraints([keyTextLeft, valueTextLeft, valueTextRight, buttonRight, keyValueEqualWidths])
+        
+        // Vertical Layout
+        var keyTextTop = NSLayoutConstraint(
+            item: keyTextField,
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Top,
+            multiplier: 1.0,
+            constant: 0.0)
+        var keyTextBottom = NSLayoutConstraint(
+            item: keyTextField, 
+            attribute: .Bottom,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Bottom,
+            multiplier: 1.0,
+            constant: 0.0)
+        var valueTextTop = NSLayoutConstraint(
+            item: valueTextField, 
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Top,
+            multiplier: 1.0,
+            constant: 0.0)
+        var valueTextBottom = NSLayoutConstraint(
+            item: valueTextField,
+            attribute: .Bottom,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Bottom,
+            multiplier: 1.0,
+            constant: 0.0)
+        var enterButtonTop = NSLayoutConstraint(
+            item: enterButton,
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Top,
+            multiplier: 1.0,
+            constant: 0.0)
+        var enterButtonBottom = NSLayoutConstraint(
+            item: enterButton,
+            attribute: .Bottom,
+            relatedBy: .Equal,
+            toItem: tableView.tableHeaderView,
+            attribute: .Bottom,
+            multiplier: 1.0,
+            constant: 0.0)
+        
+        tableView.tableHeaderView?.addConstraints([keyTextTop, keyTextBottom, valueTextTop, valueTextBottom, enterButtonTop, enterButtonBottom])
+        
+    }
+    
+    func enterButtonPressed() {
+        keyValueDictionaryArray[keyTextField.text] = valueTextField.text
+        keyTextField.text = ""
+        keyTextField.resignFirstResponder()
+        valueTextField.text = ""
+        valueTextField.resignFirstResponder()
+        tableView.reloadData()
+    }
+    
+    // MARK: Table View
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return keyValueDictionaryArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Map") as? UITableViewCell ?? UITableViewCell(style: .Value1, reuseIdentifier: "Map")
+        let row = indexPath.row
+        cell.textLabel?.text = keyValueDictionaryArray.keys[row]
+        cell.detailTextLabel?.text = keyValueDictionaryArray[keyValueDictionaryArray.keys[row]]!
+        cell.backgroundColor = backgroundColor
+        cell.textLabel?.textColor = UIColor.whiteColor()
+
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        // Needed to customize cell edge insets to expand across the screen
+        cell.separatorInset = UIEdgeInsetsZero
+        cell.superview?.preservesSuperviewLayoutMargins = false
+        cell.layoutMargins = UIEdgeInsetsZero
+       // cell.layer.borderWidth = 1
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // TODO: Delete from Ordered Dict.  Maybe create a temp then copy to original, or add removeAtIndex method to OrdDict?
+            //tableArray.removeAtIndex(indexPath.row)
+            //tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
     }
 }
+
+
